5a5e1ed2a544b9f615e131c4e7e138cc500aa449
Wigsters <wigsters25@gmail.com>
2024-09-13 15:50:32 +0100

    Completed the script to create a book report containing the number of words and characters in a book

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-        print("DONE")
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    #print(f"{num_words} words found in the document")
+    char_dict = get_character_count(text)
+    #print(char_dict)
+    bookreport(num_words, char_dict)
 
+def get_book_text(path):
+        with open(path) as f:
+            return f.read()
+
+def get_num_words(text):
+        words = text.split()
+        return len(words)
+
+def get_character_count(text):
+    chardict = {}
+    lowercase_text = text.lower()
+    for character in lowercase_text:
+        if character in chardict.keys():
+            chardict[character] += 1
+        else:
+            chardict[character] = 1
+    return chardict
+
+def bookreport(num_words, char_dict):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    char_dict_list = []
+    for key in char_dict:
+        if key.isalpha():
+            char_dict_list.append({"char": key, "num": char_dict[key]})
+    char_dict_list.sort(reverse=True, key=sort_on)
+    print()
+    for dict in char_dict_list:
+        char = dict["char"]
+        num = dict["num"]
+        print(f"The {char} character was found {num} times")
+    print("--- End report ---")
+
+def sort_on(dict):
+    return dict["num"]
 main()
